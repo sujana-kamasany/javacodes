c5e31c3360a0de8862c2507a3ca7dd19163c361c
ChenHaowei2008 <haoweichenchw@gmail.com>
2022-02-07 17:53:02 +0800

    Added floatMultiply.java and fixed issue in printinteger.java

---
+++ Basics/floatMultiply.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class floatMultiply {
+
+    public static void main(String[] args){
+        //Creates two scanners, one for each float
+        Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        
+        //Asks for the floats to multiply
+        System.out.println("Enter the first float: ");
+        Float float1 = input1.nextFloat();
+        System.out.println("Enter the second float: ");
+        Float float2 = input2.nextFloat();
+
+        //Closing the scanners is generally a good practice
+        input1.close(); 
+        input2.close();
+
+        // println() prints the following line to the output screen
+        System.out.println("The product is: " + float1 * float2);
+    }
+}
--- Basics/printinteger.java
+++ Basics/printinteger.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HelloWorld {
+public class printinteger {
 
     public static void main(String[] args) {
 
@@ -8,10 +8,11 @@ public class HelloWorld {
         // input from standard input - keyboard
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter a number: ");
-
+    
         // nextInt() reads the next integer from the keyboard
         int number = reader.nextInt();
-
+        // close() closes the reader, its a good practice
+        reader.close();
         // println() prints the following line to the output screen
         System.out.println("You entered: " + number);
     }
