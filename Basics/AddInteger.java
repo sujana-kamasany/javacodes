9eaae5c97c5c8bbac783fd6b02793735ad2ea1a8
Valan Baptist Mathuranayagam <valanm22@gmail.com>
2022-02-07 18:24:35 +0530

    Add AddInteger.java

---
+++ Basics/AddInteger.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AddInteger {
+
+    public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+
+        // nextInt() reads the first integer from the keyboard
+        int numberOne = reader.nextInt();
+
+        System.out.print("Enter number 2: ");
+
+        // nextInt() reads the second integer from the keyboard
+        int numberTwo = reader.nextInt();
+
+        // Declaring a variable to hold the sum of two integers
+        int result = numberOne + numberTwo;
+
+        // println() prints the result  to the output screen
+        System.out.println("Total is: " + result);
+    }
+}
