e4319b69a8ed73bb41478bf9044f74d898e79078
Awais khatab <42613988+AKhatabdev@users.noreply.github.com>
2022-02-11 14:07:11 +0000

    - Refactor of Scanner Util
    Implemented util.Scanner best practices (auto closure)

--- Basics/OddOrEvenNum.java
+++ Basics/OddOrEvenNum.java
@@ -1,5 +1,3 @@
-package JavaTut;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,28 +7,26 @@ public class OddOrEvenNum {
         long num;
 
         //Initialise Scanner
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            while (!complete) {
+                try {
+                    //User input
+                    System.out.println("Enter a number:");
+                    num = input.nextLong();
 
-        while (!complete) {
-            try {
-                //User input
-                System.out.println("Enter a number:");
-                num = input.nextLong();
-
-                //IF input is NOT divisible by 2 then it's an ODD number
-                //ELSE it's an EVEN number
-                if (num % 2 != 0) {
-                    System.out.println(num + " is an odd number");
-                } else {
-                    System.out.println(num + " is an even number");
+                    //IF input is NOT divisible by 2 then it's an ODD number
+                    //ELSE it's an EVEN number
+                    if (num % 2 != 0) {
+                        System.out.println(num + " is an odd number");
+                    } else {
+                        System.out.println(num + " is an even number");
+                    }
+                    complete = true; //Task completed
+                } catch (InputMismatchException e) {
+                    System.out.println("INPUT NOT VALID: Please enter a number!");
+                    input.nextLine();
                 }
-                complete = true; //Task completed
-            } catch (InputMismatchException e) {
-                System.out.println("INPUT NOT VALID: Please enter a number!");
-                input.nextLine();
             }
         }
-        //Close Scanner
-        input.close();
     }
 }
