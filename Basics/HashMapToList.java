921d18192645bc76d87fcef8005796078b1b58a5
Adam Sadek <33073174+AdamSadek@users.noreply.github.com>
2022-02-12 18:37:23 +0000

    Create HashMapToList.java
    Converting a HashMap to a List

---
+++ Basics/HashMapToList.java
@@ -0,0 +1,10 @@
+public class HashMapToList {
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Hello", 0);
+        map.put("Word", 1);
+        List<String> keys = new ArrayList<>(map.keySet());
+        List<Integer> values = new ArrayList<>(map.values());
+    }
+}
