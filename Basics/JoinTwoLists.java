63225d81fef90bf49f2ebdc61add371d2cda34cf
anipaul2 <aniketh131@gmail.com>
2022-02-13 17:15:18 +0530

    issue#51 solved

---
+++ Basics/JoinTwoLists.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class JoinTwoLists {
+    public static void main(String[] args) {
+
+        List<String> list1 = new ArrayList<String>();
+        list1.add("a");
+        List<String> list2 = new ArrayList<String>();
+        list2.add("b");
+
+        List<String> joined = new ArrayList<String>();
+        joined.addAll(list1);
+        joined.addAll(list2);
+
+        System.out.println("list1: " + list1);
+        System.out.println("list2: " + list2);
+        System.out.println("joined: " + joined);
+    }
+}
