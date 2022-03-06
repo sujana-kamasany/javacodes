d09f8006565113f0450b91520926eaecfeed099c
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-03-06 22:16:10 +0900

    Add Spy nubmer comment

--- Basics/SpyNumber/SpyNumber.java
+++ Basics/SpyNumber/SpyNumber.java
@@ -2,6 +2,15 @@ package JavaTut.SpyNumber;
 
 import java.util.Scanner;
 
+/**
+ * 	SpyNumber
+ * 	sum of each digit is equals to product of the digits.
+ * 	ex) 1124
+ * 	sum of digits = 1 + 1 + 2 + 4
+ * 	= 6
+ * 	product of the digits = 1 * 1 * 2 * 4
+ * 	= 6
+ */
 public class SpyNumber {
 
 	public static void main(String[] args) {
