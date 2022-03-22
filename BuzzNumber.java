ddd7d3f6bdf1634c5452d95cae68c6c034150236
Jason Rodriguez <86337121+jrodriq3@users.noreply.github.com>
2022-03-22 13:40:07 -0400

    Update BuzzNumber.java
    Hello, This is my first commit... Im sorry if i did it wrong. I added functionality so that if number entered is negative it will prompt user to re-enter a positive number. Also added funtionality in a try catch block so that if user enters a non number it prompts user to enter a number before it would have errored out.

--- BuzzNumber.java
+++ BuzzNumber.java
@@ -3,13 +3,35 @@ import java.util.Scanner;
 public class BuzzNumber {
 
     public static void main(String[] args) {
-        System.out.println("Input an integer: "); // Assumes input is a valid non-negative integer
-	    Scanner in = new Scanner(System.in);
-        int checkBuzz = in.nextInt();
-        boolean isBuzzNumber = false;
-        if (checkBuzz % 7 == 0 || checkBuzz % 10 == 7){ // Checks if input can be divisible by 7 or ends with 7
-            isBuzzNumber = true;
+        boolean isPositive = false;
+        // while loop and isPositive flag will assure integer entered is positive
+        while (isPositive == false) {
+            int checkBuzz = -1;
+            System.out.println("Input an integer: ");
+            boolean validEntry = false;
+            // while loop with try catch to make sure input is valid integer
+            while (validEntry == false) {
+                try {
+                    Scanner in = new Scanner(System.in);
+                    checkBuzz = in.nextInt();
+                    validEntry = true;
+                } catch (Exception e) {
+                    System.out.println("You didn't enter an integer. Re-enter...");
+                }
+            }
+            // if statement to continue loop if input was a negative number.
+            if (checkBuzz < 0) {
+                continue;
+            }
+            boolean isBuzzNumber = false;
+            if (checkBuzz % 7 == 0 || checkBuzz % 10 == 7){ // Checks if input can be divisible by 7 or ends with 7
+                isBuzzNumber = true;
+            }
+            isPositive = true;
+            System.out.println("isBuzzNumber: " + isBuzzNumber);
+
         }
-        System.out.println(isBuzzNumber);
+        
+        
     }
 }
