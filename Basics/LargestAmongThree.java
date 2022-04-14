cbfd871946266ea1d4aeb5c7b92bdc22dcbe3d1d
unknown <you@example.com>
2022-04-14 17:31:23 +0530

    Enhanced

--- Basics/LargestAmongThree.java
+++ Basics/LargestAmongThree.java
@@ -1,30 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 
-import javax.lang.model.util.ElementScanner6;
-
-class LargestAmongThree
-{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter The First Number :-");
-        int a=sc.nextInt();
-        System.out.print("\nEnter The second Number :-");
-        int b=sc.nextInt();
-        System.out.print("\nEnter The Third Number :-");
-        int c=sc.nextInt();
-
-        if(a>b && a>c)
-        {
-            System.out.print("\nThe Largest Number is :"+a);
-        }
-        else if(b>c)
-        {
-            System.out.print("\nThe Largest Number is :"+b);
-        }
-        else 
-        {
-            System.out.print("\nThe Largest Number is :"+c);
-        }
-        
-    }    
+public class LargestAmongThree {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the first numbers:");
+		int firstNumber = scanner.nextInt();
+		System.out.println("Enter the second numbers:");												//Enter the oneNumber/line
+		int secondNumber = scanner.nextInt();
+		System.out.println("Enter the third numbers:");
+		int thirdNumber = scanner.nextInt();
+		System.out.println("The largest Number is: " + (firstNumber > secondNumber && firstNumber > thirdNumber ? firstNumber : secondNumber > thirdNumber ? secondNumber : thirdNumber));
+	}
 }
