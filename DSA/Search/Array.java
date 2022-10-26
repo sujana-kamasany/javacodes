89aa10bceb4b2b213221f7a9c8a7f22aafbb3de5
tadiharshith <116142975+tadiharshith@users.noreply.github.com>
2022-10-26 19:18:03 +0530

    Create Array.java

---
+++ DSA/Search/Array.java
@@ -0,0 +1,27 @@
+
+class GFG {
+	public static void main(String[] args)
+	{
+		
+		int[] arr;
+
+		
+		arr = new int[5];
+
+		
+		arr[0] = 10;
+
+		
+		arr[1] = 20;
+
+		// so on...
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+
+
+		for (int i = 0; i < arr.length; i++)
+			System.out.println("Element at index " + i
+							+ " : " + arr[i]);
+	}
+}
